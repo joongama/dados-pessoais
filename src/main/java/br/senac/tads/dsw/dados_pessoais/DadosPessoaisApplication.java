56a143302b6e84a7aa259d29ba2274d5fcf51493
joongama <joaopedrogama2704@gmail.com>
2026-09-22 17:00:45 -0300

    Etapa 00

---
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplication.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 144ae5a5fcc42f423c535384eb71cc0bad1ed4a0e57867376274bc8a75052369
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplicationTests.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 144ae5a5fcc42f423c535384eb71cc0bad1ed4a0e57867376274bc8a75052369
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
